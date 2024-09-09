6ad4a096120392d7d33910cd6ba4382a30ef3a25
kasakj <kubakasak@seznam.cz>
2024-09-09 07:56:56 +0200

    stoff

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        ArrayList<Mazlicek> pets = new ArrayList<>();
+        System.out.println("Hello and welcome!");
+
         }
     }
---
+++ src/Mazlicek.java
@@ -0,0 +1,56 @@
+public class Mazlicek {
+    public String name;
+    public String druh;
+    public int age;
+    public double weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDruh() {
+        return druh;
+    }
+
+    public void setDruh(String druh) {
+        this.druh = druh;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Mazlicek(String name, String druh, int age, double weight) {
+        this.name = name;
+        this.druh = druh;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Mazlicek{" +
+                "name='" + name + '\'' +
+                ", druh='" + druh + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                '}';
+    }
+}
+
